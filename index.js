15c6158a53244995ab2030eb23dfc52063e980b1
Frank <frank.next@gmx.de>
2018-01-17 18:56:27 +0100

    added code in preparation of live-listeners for actions

--- index.js
+++ index.js
@@ -13,34 +13,88 @@
  * TODO make sure that there may not be more than one action defined and throw error otherwise
  * TODO class and package structure
  */
+
+import {onElementChange} from "./listeners"
+import {assignIn as extend} from 'lodash';
+
 import Mousetrap from 'mousetrap';
 import $ from 'jquery';
 
 var _keys = {};
+
 /**
  *
- * @param action
- * @param {(string|array)}combo -  eg. ctrl+s
+ * @param action -  TODO action shoud contain the category string
+ * @param {(string|array)}combo - one or many combos eg. ctrl+s TODO touch gestures would be nice for customisation
  * @param handler - based on extra param is handler for keypress or keydown event
  * @param extra - if extra is a function, it will be interpreted as keyup event  and the handler will be keydown event else handler is keypress
+ * @param options - contains additional options, might be passed in place of param 'extra' if no keyup handler is needed
+ * @param {(string|array[string])} options.category -  example: "Default.save File" or array of string ["Default","File safe"]
+ * @param {(HTMLElement|undefined)} options.target -  the element to bind the action to
+ * @param {string} options.selector -   selector-string for binding existing or later added elements
+ * @param {string} options.description -  some text with additional information about the binding
  * @constructor
+ *
  */
-export function Hotkeys(action, combo, handler, extra = null) {
+export function Hotkeys(action, combo, handler, extra = null, options) {
+
+    if (typeof extra == "object")
+        options = extra;
+
+
+    var defaults = {
+        category: "",
+        target: window.document,
+        selector: null,
+        description: "-/-"
+    }
+
+
+    options = extend(defaults, options)
+
+
+    var hasSecondHandler = typeof extra == "function";
+
     if (_keys[combo]) {
         console.error("key '" + combo + "' already set");
         return;
     }
 
-    _keys[combo + '-' + extra] = {action, combo, handler, extra: extra ? 'keypress' : 'up/down'};
+    var t = hasSecondHandler ? 'up/down' : 'keypress'
 
-    if (!extra) {
-        Mousetrap.bind(combo, handler);
-    } else {
-        Mousetrap.bind(combo, handler, 'keydown');
-        Mousetrap.bind(combo, extra, 'keyup');
+    _keys[combo + '-' + t] = {action, combo, handler, extra: t};
+
+
+    function applyHandlers(target) {
+        if (!hasSecondHandler) {
+            Mousetrap(target).bind(combo, handler);
+        } else {
+            Mousetrap(target).bind(combo, handler, 'keydown');
+            Mousetrap(target).bind(combo, extra, 'keyup');
+        }
     }
+
+
+    applyHandlers(options.target)
+
+
+    if (options.selector != null)
+        onElementChange(options.target, options.selector, function onTargetChanged(arr) {
+
+            console.log("hotkeys target ...", arguments)
+
+            applyHandlers(arr[0].added[0])
+
+        })
+
+
 }
 
+
+export function getRegistered() {
+    return _keys
+
+}
 
 export function showHotkeyList() {
     var style = `
