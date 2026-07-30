e3184d0223444ee45d28896ece4b39f87f19be14
Frank <frank.reimann@jambit.de>
2026-07-30 23:42:12 +0200

    dedupe react

--- ladle/.ladle/vite.config.mjs
+++ ladle/.ladle/vite.config.mjs
@@ -1,7 +1,18 @@
 import { defineConfig } from "vite";
+import { fileURLToPath } from "url";
+import path from "path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   base: "/input-device-actions/",
+  resolve: {
+    dedupe: ["react", "react-dom", "svelte"],
+    alias: {
+      react: path.resolve(__dirname, "../node_modules/react"),
+      "react-dom": path.resolve(__dirname, "../node_modules/react-dom"),
+    },
+  },
   optimizeDeps: {
     include: ["svelte", "svelte/internal/client"],
   },
